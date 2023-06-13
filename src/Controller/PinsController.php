84a3d3378784e2a81796ba5dfbd427c40d4d300a
matt <matthieugarban@gmail.com>
2023-06-13 10:36:45 +0200

    add edit & pbl put/post 4 edit

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -3,9 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\Pin;
+use App\Form\PinType;
 use App\Repository\PinRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PinsController extends AbstractController
@@ -28,6 +34,112 @@ class PinsController extends AbstractController
 
         return $this->render('pins/show.html.twig', compact('pin'));
     }
+
+
+    //CREATE
+    #[Route('/pins/create', name: 'app_pins_create', methods: ['GET', 'POST'], priority: 10)]
+    // #[Route('/pins/create', name: 'app_pins_create', methods: ['GET', 'POST'])]
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        
+        // // version 1
+        // $form = $this->createFormBuilder()
+        //         ->add('title', TextType::class)
+        //         ->add('description', TextareaType::class)
+        //         ->getForm()
+        // ;
+
+        // $form->handleRequest($request);
+        // //formulaire récupère les données du formulaire via la requête
+
+        // if($form->isSubmitted() && $form->isValid())
+        // {
+        //     $data = $form->getData();
+        //     $pin = new Pin;
+        //     $pin->setTitle($data['title']);
+        //     $pin->setDescription($data['description']);
+
+
+        // //version 2
+        // $form = $this->createFormBuilder(new Pin)
+        //     ->add('title', TextType::class)
+        //     ->add('description', TextareaType::class)
+        //     ->getForm()
+        // ;
+
+        // $form->handleRequest($request);
+
+        // if($form->isSubmitted() && $form->isValid())
+        // {
+        //     $pin = $form->getData();
+
+
+        //  // version 3
+        // $pin = new Pin;
+        // $form = $this->createFormBuilder($pin, [
+        //     'auto_initialize' => true,])
+        //     ->add('title')
+        //     ->add('description')
+        //     ->getForm()
+        // ;
+
+        // $form->handleRequest($request);
+
+        // if($form->isSubmitted() && $form->isValid())
+        // {
+
+        // version 4
+        $pin = new Pin;
+        $form = $this->createForm(PinType::class, $pin);//on passe par le form qu'on a crée avec symfony console make:form
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+
+            $em->persist($pin);
+            $em->flush();
+
+            $this->addFlash('success', 'Pin successfully created');
+
+            return $this->redirectToRoute('app_home');
+        }
+        
+        return $this->render('pins/create.html.twig', ['form' =>$form->createView()]);
+    }
+
+    //EDIT
+    #[Route('/pins/{id}/edit', name: 'app_pins_edit', methods: ['GET' ,'PUT', 'POST'])]
+    
+    public function edit(Request $request, EntityManagerInterface $em, int $id, PinRepository $pinRepository):Response
+    {
+        $pin = $pinRepository->find($id);
+
+        $form = $this->createForm(PinType::class, $pin);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            // $em->persist($pin);
+            $em->flush();
+            $this->addFlash('success', 'Pin successfully updated');
+
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('pins/edit.html.twig', [
+            'pin' => $pin,
+            'form' => $form->createView()
+        ]);
+    }
+
+
+
+
+
+
 
 
 }
