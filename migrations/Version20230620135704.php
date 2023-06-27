8f9ee2433c6cdc2e9a23957241e4ea847b3dd166
matt <matthieugarban@gmail.com>
2023-06-27 18:40:21 +0200

    modif length key description

--- migrations/Version20230620135704.php
+++ migrations/Version20230620135704.php
@@ -7,25 +7,28 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20230620135704 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Add new columns to the users table';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE users CHANGE email email VARCHAR(255) NOT NULL');
+        $table = $schema->getTable('users');
+        $table->addColumn('first_name', 'string', ['length' => 255]);
+        $table->addColumn('last_name', 'string', ['length' => 255]);
+        $table->addColumn('email', 'string', ['length' => 255]);
+        $table->addColumn('password', 'string', ['length' => 255]);
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE users CHANGE email email VARCHAR(180) NOT NULL');
+        $table = $schema->getTable('users');
+        $table->dropColumn('first_name');
+        $table->dropColumn('last_name');
+        $table->dropColumn('email');
+        $table->dropColumn('password');
     }
 }
